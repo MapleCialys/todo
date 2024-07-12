b28584133bd02dc539715b60d28b8de9c2c6bcb9
Mickael <mickael.francois@vivaldi.net>
2024-07-11 21:59:46 -0400

    push final

---
+++ src/lib/components/boite.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let titre;
+    export let description;
+    export let priorite
+    function minus()
+    {
+        priorite -= 1;
+    };
+    function plus()
+    {
+        priorite += 1;
+    };
+</script>
+
+<div class="card">
+    <header class="card-header">
+      <p class="card-header-title">{titre}</p>
+      <button class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </button>
+    </header>
+    <div class="card-content">
+      <div class="content">
+{description}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <button class="card-footer-item" on:click={minus}>-</button>
+      <p class="card-footer-item">{priorite}</p>
+      <button class="card-footer-item" on:click={plus}>+</button>
+    </footer>
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Boite from "../lib/components/boite.svelte";
+
+    class tache
+    {
+        title;
+        description;
+        priorite;
+
+        constructor(p_title, p_description, p_priorite)
+        {
+            this.title = p_title;
+            this.description = p_description;
+            this.priorite = p_priorite;
+        };
+    }
+
+    let list = [];
+
+    function ajouter()
+    {
+        let f_titre = document.getElementById('titre').value;
+        let f_description = document.getElementById('desc').value;
+        let f_priorite = document.getElementById('priorite').value;
+        let newTask = new tache(f_titre, f_description, f_priorite);
+        console.log(newTask);
+        list = [...list, newTask];
+    };
+</script>
+<div class="columns is-centered">
+    <div class="column is-half">
+<h1 class="title">TODO</h1>
+<div class="field">
+    <label class="label">Titre</label>
+    <div class="control">
+      <input class="input" type="text" id="titre">
+    </div>
+  </div>
+  
+  <div class="field">
+    <label class="label">Description</label>
+    <div class="control">
+      <textarea name="desc" id="desc" class="textarea"></textarea>
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">Priorite</label>
+    <div class="control">
+      <input type="number" class="input" id="priorite">
+    </div>
+  </div>  
+  <div class="control">
+    <button class="button is-primary" on:click={ajouter}>Ajouter</button>
+  </div>
+    {#each list as task}
+        <Boite titre={task.title} description={task.description} priorite={task.priorite}/>
+    {/each}
+    </div>
+    </div>
